3ba40696f2126b41b53e9d708c484a010ad7a232
Max Lerman <57683212+lermanMax@users.noreply.github.com>
2021-05-04 16:30:26 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
-API_TOKEN = '1638449618:AAHEjvQyAxRAFSgaXkpZv2gm77eGj3Man28'
+API_TOKEN = ''
 
 
 DB_HOST = "localhost"
-DB_NAME = "feedback_db"
+DB_NAME = "menu_db"
 DB_USER = "bot"
 DB_PASS = "bot"
 DB_PORT = "5432"  
 
-admin_id = [996991853, 382758093, 98244574]
+admin_id = [996991853,]
